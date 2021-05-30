a12be411f11ed0dafd9fa7b138ae0af230172607
RomanKaktysh <biggfall21@gmail.com>
2021-05-30 16:50:24 +0300

    version

--- src/Grafic.java
+++ src/Grafic.java
@@ -12,9 +12,11 @@ public class Grafic {
 
 	public static void main(String[] args) {
 
+		
 		JFrame frame = new JFrame();
 		frame.setSize(500, 500);
 		frame.setTitle("Window");
+		frame.setAlwaysOnTop(true);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container container = frame.getContentPane();
